4a1a83301e391c05d9103777554a7200694b56c3
dmosborne <160295634+dmosborne@users.noreply.github.com>
2025-06-12 18:43:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,100 +1,89 @@
-const bodyParser = require("body-parser");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
+let yourName = [];
+let addPost = [];
 
-let yourName =[];
-let addPost =[];
+app.get("/", (req, res) => {
+  const d = new Date().toLocaleDateString("en-US", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    year: "numeric"
+  });
 
-app.get("/", (_req, res) =>{
-    const d = new Date().toLocaleDateString("en-US", {
-       weekday: 'long',
-       month: 'long',
-       day: 'numeric',
-       year: 'numeric',
-       
-    });
-
-        
-    res.render("index.ejs", {
-    day:d,
+  res.render("index.ejs", {
+    day: d,
     newPost: addPost,
-    author: yourName,
-    
-    });
-    });
-    
-
-app.post("/submit", (req, res) => {
-const newPost = req.body.newPost;    
-    if (newPost.trim()!==""){
-    addPost.push(newPost);
-    }
-
-const author = req.body.author;
-    if (author.trim()!==""){
-    yourName.push(author);
-    }
-
-    res.redirect("/");
+    author: yourName
+  });
 });
 
-app.get('/blogs', (_req, res) => {
+app.post("/submit", (req, res) => {
+  const newPost = req.body.newPost;
+  if (newPost.trim() !== "") {
+    addPost.push(newPost);
+  }
 
-const d = new Date().toLocaleDateString("en-US", {
-        weekday: 'long',
-        month: 'long',
-        day: 'numeric',
-        year: 'numeric',
-        });
+  const author = req.body.author;
+  if (author.trim() !== "") {
+    yourName.push(author);
+  }
 
-    res.render('blogs.ejs', {
-        title: 'View Posts',
-        day: d,
-        newPost: addPost,
-        author: yourName,
+  res.redirect("/");
+});
 
-        });
-    });
+app.get("/blogs", (req, res) => {
+  const d = new Date().toLocaleDateString("en-US", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    year: "numeric"
+  });
 
+  res.render("blogs.ejs", {
+    title: "View Posts",
+    day: d,
+    newPost: addPost,
+    author: yourName
+  });
+});
 
 app.get("/edit-blog/:id", (req, res) => {
-    const blogId = parseInt(req.params.id);
-    const blog = addPost[blogId];
+  const blogId = parseInt(req.params.id);
+  const blog = addPost[blogId];
 
-
-    if (blog) {
-        res.render("edit-blogs.ejs", { blog, blogId });
-    } else {
-        res.redirect("/blogs");
-    }
-    });
+  if (blog) {
+    res.render("edit-blogs.ejs", { blog, blogId });
+  } else {
+    res.redirect("/blogs");
+  }
+});
 
 app.post("/update-blog/:id", (req, res) => {
-    const blogId = parseInt(req.params.id);
-    const newPost = req.body.newPost;
+  const blogId = parseInt(req.params.id);
+  const newPost = req.body.newPost;
 
-    if (newPost) {
-        addPost[blogId] = newPost;
-    }
-    
-    res.redirect("/blogs");
+  if (newPost) {
+    addPost[blogId] = newPost;
+  }
+
+  res.redirect("/blogs");
 });
 
 app.post("/delete-blog/:id", (req, res) => {
-    const blogId = parseInt(req.params.id);
-    addPost.splice(blogId, 1);
-    res.redirect("/blogs");
+  const blogId = parseInt(req.params.id);
+  addPost.splice(blogId, 1);
+  res.redirect("/blogs");
 });
 
-
-app.listen(PORT, '0.0.0.0' () => {
-console.log (`Listening on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
